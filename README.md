bbef47b05bdba573919a0a41c471e75069fc5205
Kenneth Wußmann <kenneth@wussmann.net>
2019-02-24 17:04:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Cognito CLI
+Small CLI tool to obtain a JWT from a Cognito userpool. Supports multiple userpools ordered by stages.
+
+# Usage
+* Clone repo & `npm install`
+* Rename `example.config.json` to `config.json`
+* Provide credentials in the config.json
+* `npm start`
+
+**Shows list of applications configured**
+```
+? What pool type would you like to use? (Use arrow keys)
+❯ Application 1
+  Application 2
+```
+
+**Shows available stages for this application**
+```
+? What pool type would you like to use? Driver
+? And for what stage?
+  dev
+❯ int
+  prd
+```
+
+**Copies the obtained JWT to your clipboard (macOS, Linux & Windows)** 
+```
+Copied JWT for Application 1 INT to clipboard!
+```
---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+var ncp = require("copy-paste");
+var inquirer = require('inquirer');
+var config = require('./config.json');
+
+console.log("");
+
+inquirer
+    .prompt({
+        type: "list",
+        name: "poolType",
+        message: "What pool type would you like to use?",
+        choices: config.pools.map(pools => pools.name)
+    })
+    .then(poolTypeAnswers => {
+        var pool = config.pools.filter(pools => pools.name == poolTypeAnswers.poolType)[0];
+        var stages = Object.keys(pool).filter(key => key != "name")
+        inquirer
+            .prompt({
+                type: 'list',
+                name: 'stage',
+                message: 'And for what stage?',
+                choices: stages
+            })
+            .then(stageAnswers => {
+                var stage = pool[stageAnswers.stage];
+                auth(stage)
+                    .catch(err => {
+                        console.error(`\nFailed to get JWT: ${err.code} - ${err.message}`);
+                    })
+                    .then(jwt => {
+                        if (jwt != null) {
+                            console.log(`\n${jwt}`);
+                            ncp.copy(jwt, () => {
+                                console.log(`\nCopied JWT for ${poolTypeAnswers.poolType} ${stageAnswers.stage.toUpperCase()} to clipboard!`);
+                            });
+                        }
+                    });
+            });
+    });
+
+function auth(stage) {
+    return new Promise((resolve, reject) => {
+        global.fetch = require('node-fetch')
+        var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+        var cognitoUser = new AmazonCognitoIdentity.CognitoUser({
+            Username: "username",
+            Pool: new AmazonCognitoIdentity.CognitoUserPool({
+                UserPoolId: stage.poolId,
+                ClientId: stage.clientId
+            })
+        });
+        cognitoUser.authenticateUser(new AmazonCognitoIdentity.AuthenticationDetails({
+            Username: stage.username,
+            Password: stage.password,
+        }), {
+            onSuccess: (result) => {
+                resolve(result.getAccessToken().getJwtToken());
+            },
+            onFailure: (err) => reject(err)
+        });
+    });
+}
